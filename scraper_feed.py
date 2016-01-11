070ac93916d66af1bcd91e403eef91254abe43e1
Onur H <onurmhb@gmail.com>
2016-01-11 02:55:37 +0100

    added feedparser

--- scraper_feed.py
+++ scraper_feed.py
@@ -1,20 +1,17 @@
-# coding=utf-8
-from bs4 import BeautifulSoup
-import requests
-from pymongo import MongoClient
 from multiprocessing.pool import ThreadPool
-import threading
-import datetime
+from pymongo import MongoClient
+from bs4 import BeautifulSoup
+import feedparser
+import pprint
+import requests
 import time
 
-##
-# Setup
-##
+#################################################################################################
 num_threads = 4
-
 client = MongoClient('localhost', 27017)
 db = client.api #name of db
 url_db = db['urls']
+
 try:
 	collection = db.create_collection('articles', capped = True, max = 3000, size = 5242880)
 except:
@@ -34,52 +31,37 @@ def get_image(url):
 		image_meta = soup.find(property = 'og:image') or soup.find(name = 'twitter:image')
 		return image_meta.get('content')
 	except:
-		image_meta = ""
-		return image_meta
+		return ""
 
 
-def thread_work(url):
-	date = datetime.datetime.now()
-	#parse url
-	try:
-		req = requests.get(url.get('link'), headers=request_headers)
-	except:
-		return None
-	soup = BeautifulSoup(req.content, 'xml')
-
-	for item in soup.findAll('item')[0:3]:
+def worker(url):
+	feed = feedparser.parse(url.get('link'))
+	items = feed['items']
+	for item in items[0:3]:
 		article = {}
-		article['title'] = item.title.text.encode('utf-8')
+		article['title'] = item['title'].encode('utf-8')
 		if collection.find({'title':article.get('title')}).count() == 0:
-
-			article['link'] = item.link.text.encode('utf-8')
-			article['pubdate'] =  date.isoformat()
+			article['date'] = item['published'].encode('utf-8')
 			article['provider'] = url.get('provider')
 			article['country'] = url.get('country')
 			article['category'] = url.get('category')
-			try:
-				article['tag'] = item.category.text.encode('utf-8')
-			except:
-				article['tag'] = ""
-			try:
-				article['image_url'] =  item.image.url.text.encode('utf-8')
-			except Exception as e:
-				article['image_url'] =  get_image(item.link.text.encode('utf-8'))
-			print article.get('title')
+			article['image_url'] = get_image(item['link'])
 			collection.insert_one(article)
+			print article.get('title')
+
 
 
 def init():
-	#GET urls from db
 	while True:
-		urls = [url for url in url_db.find({})]  #urls to parse in bjson format
-		time.sleep(3)
 		start = time.time()
+		urls = [url for url in url_db.find({})]
+		print "Process started with %s urls" % len(urls)
 		pool = ThreadPool(num_threads)
-		pool.map(thread_work, urls)
+		pool.map(worker, urls)
 		pool.close()
 		pool.join()
-		print "* Process Finished in: ", time.time() - start
+		print "-----------"
+		print "Process finished in: %s" % (time.time() - start)
 		time.sleep(3)
 
 if __name__ == '__main__':
