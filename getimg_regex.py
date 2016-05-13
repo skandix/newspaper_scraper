7957592c1f90e520705e61fea25934eac956759b
Bendik <skandix@users.noreply.github.com>
2016-05-13 21:00:05 +0200

    Create getimg_regex.py
    finding img url from sites, with regex, regex for finding the url from header
    
    Test input
    <meta property="og:image" content="http://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2016/05/13/0/0/694940094001_4892426916001_051316-Dotcom-Bathroom.jpg?ve=1">
    
    Test output
    http://a57.foxnews.com/media2.foxnews.com/BrightCove/694940094001/2016/05/13/0/0/694940094001_4892426916001_051316-Dotcom-Bathroom.jpg?ve

---
+++ getimg_regex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import urllib
+import re
+
+img_link = []
+
+def getIMG(url):
+	#regex magix for matching images that has og:image or twitter or facebook
+	p = re.compile(ur'(?!<\w{4}\s\w{8}\S+)(?:image|twitter|facebook)"\s\w{7}\S{2}[\w\d\S]+[.jpg|.png|.gif|.jpeg|]')
+
+	#opening site
+	f = urllib.urlopen(url)
+
+	#reading html code
+	live_html = f.read() 
+
+	#using regex on html code
+	loot = re.findall(p, live_html)
+
+	#adds to list
+	img_link.append(loot)
+
+	#iterates through list, and takes away shit stuff, and takes the first in the list. and returns it
+	for i in img_link:
+		return str(i[0]).replace('image" content="', '')
+
+print getIMG("http://www.foxnews.com/politics/2016/05/13/conservatives-outraged-over-obama-transgender-directive-to-public-schools.html?intcmp=hpbt1")
